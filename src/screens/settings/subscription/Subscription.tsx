21f03e7926938013d1c0a36dff61547383ea81ce
mdmidulhosen <muhammadmridulhosenkibria@gmail.com>
2024-11-21 21:16:12 +0600

    subscription screen dark mode done

--- src/screens/settings/subscription/Subscription.tsx
+++ src/screens/settings/subscription/Subscription.tsx
@@ -12,12 +12,12 @@ import {RadioButton, RadioGroup} from 'react-native-ui-lib';
 const Subscription = ({navigation}: any) => {
   const [subscription, setSubscription] = useState(0);
   return (
-    <View style={tw`bg-white h-full px-[4%] pb-2`}>
+    <View style={tw`bg-white h-full px-[4%] pb-2 dark:bg-primaryDark`}>
       <ScrollView style={tw`mt-3`}>
         {/* Header */}
         <View style={tw`items-start`}>
           <TouchableOpacity
-            style={tw`h-12 w-12 rounded-full bg-white items-center justify-center border border-gray90`}
+            style={tw`h-12 w-12 rounded-full bg-white dark:bg-darkBg items-center justify-center border border-gray90 dark:border-darkBg`}
             onPress={() => {
               navigation?.goBack();
             }}>
@@ -28,26 +28,29 @@ const Subscription = ({navigation}: any) => {
         <View style={tw`items-center mt-4`}>
           <Image source={require('../../../assets/images/diamond.png')} />
           <Text
-            style={tw`text-black text-2xl font-WorkBold font-700 leading-9 mt-4 text-center`}>
+            style={tw`text-black dark:text-white text-2xl font-WorkBold font-700 leading-9 mt-4 text-center`}>
             Unlock Premium {'\n'}Travel Experience
           </Text>
           {/* package details list */}
           <View style={tw`mt-8 gap-y-4`}>
             <View style={tw`flex-row items-center gap-3`}>
               <SvgXml xml={IconVerifiedTik} />
-              <Text style={tw`text-gray70 text-base font-WorkMedium font-500`}>
+              <Text
+                style={tw`text-gray70 dark:text-white text-base font-WorkMedium font-500`}>
                 Unlimited bucketlist items
               </Text>
             </View>
             <View style={tw`flex-row items-center gap-3`}>
               <SvgXml xml={IconVerifiedTik} />
-              <Text style={tw`text-gray70 text-base font-WorkMedium font-500`}>
+              <Text
+                style={tw`text-gray70 dark:text-white text-base font-WorkMedium font-500`}>
                 Premium customization features
               </Text>
             </View>
             <View style={tw`flex-row items-center gap-3`}>
               <SvgXml xml={IconVerifiedTik} />
-              <Text style={tw`text-gray70 text-base font-WorkMedium font-500`}>
+              <Text
+                style={tw`text-gray70 dark:text-white text-base font-WorkMedium font-500`}>
                 Ad-free experience
               </Text>
             </View>
@@ -57,62 +60,65 @@ const Subscription = ({navigation}: any) => {
             <TouchableOpacity
               style={tw`flex-row items-center justify-between w-full px-4 ${
                 subscription === 0
-                  ? 'bg-radioBg border-violet100'
-                  : 'bg-transparent border-gray90'
+                  ? 'bg-radioBg dark:bg-violet100 border-violet100'
+                  : 'bg-transparent border-gray90 dark:border-violet100'
               } border  rounded-2xl h-16`}
               onPress={() => setSubscription(0)}>
               <View style={tw`flex-row items-center gap-3`}>
                 <View
                   style={tw`border ${
-                    subscription === 0 ? 'border-violet100' : 'border-gray90'
+                    subscription === 0
+                      ? 'border-violet100'
+                      : 'border-gray90 dark:border-violet100'
                   } rounded-full`}>
                   <View
                     style={tw`border-[6px] ${
                       subscription === 0
-                        ? 'border-violet100'
+                        ? 'border-violet100 dark:border-white'
                         : 'border-transparent'
                     } h-6 w-6 rounded-full`}
                   />
                 </View>
                 <Text
-                  style={tw`text-black text-[20px] font-WorkSemiBold font-600`}>
+                  style={tw`text-black dark:text-white text-[20px] font-WorkSemiBold font-600`}>
                   Monthly
                 </Text>
               </View>
-              <Text style={tw`text-black text-base font-WorkMedium font-500`}>
+              <Text
+                style={tw`text-black dark:text-white text-base font-WorkMedium font-500`}>
                 $29.99/year
               </Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={tw`flex-row items-center justify-between w-full px-4 ${
                 subscription === 1
-                  ? 'bg-radioBg border-violet100'
-                  : 'bg-transparent border-gray90'
+                  ? 'bg-radioBg border-violet100 dark:bg-violet100'
+                  : 'bg-transparent border-gray90 dark:border-violet100'
               } border  rounded-2xl h-16`}
               onPress={() => setSubscription(1)}>
               <View style={tw`flex-row items-center gap-3`}>
                 <View
                   style={tw`border ${
-                    subscription === 1 ? 'border-violet100' : 'border-gray90'
+                    subscription === 1 ? 'border-violet100' : 'border-gray90 dark:border-violet100'
                   } rounded-full`}>
                   <View
                     style={tw`border-[6px] ${
                       subscription === 1
-                        ? 'border-violet100'
+                        ? 'border-violet100 dark:border-white'
                         : 'border-transparent'
                     } h-6 w-6 rounded-full`}
                   />
                 </View>
                 <Text
-                  style={tw`text-black text-[20px] font-WorkSemiBold font-600`}>
+                  style={tw`text-black dark:text-white text-[20px] font-WorkSemiBold font-600`}>
                   Annually
                 </Text>
               </View>
-              <Text style={tw`text-black text-base font-WorkMedium font-500`}>
+              <Text style={tw`text-black dark:text-white text-base font-WorkMedium font-500`}>
                 $29.99/year
               </Text>
               <View
-                style={tw`bg-violet100 rounded px-2 py-1 absolute top-[-3] right-7`}>
+                style={tw`bg-violet100 dark:bg-darkBg border border-transparent dark:border-violet100 rounded px-2 py-1 absolute top-[-3] right-7`}>
                 <Text style={tw`text-white text-xs font-WorkMedium font-500`}>
                   🔥 Save 15%
                 </Text>
