7a95fa625e36f18ff76169169e3c3e66bf4236b5
mdmidulhosen <muhammadmridulhosenkibria@gmail.com>
2024-12-01 16:47:59 +0600

    updated code

--- src/screens/policyAndAbout/Faq.tsx
+++ src/screens/policyAndAbout/Faq.tsx
@@ -10,15 +10,17 @@ const Faq = ({navigation}: any) => {
   return (
     <View style={tw`flex-1 bg-white px-[4%] pt-4 dark:bg-primaryDark`}>
       <View style={tw`flex-row`}>
-        <TouchableOpacity onPress={() => navigation.goBack()}
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
           style={tw`h-12 w-12 rounded-full bg-white dark:bg-darkBg items-center justify-center border border-gray90 dark:border-darkBg`}>
           <SvgXml xml={IconLeftArrow} />
         </TouchableOpacity>
       </View>
 
       <View style={tw`px-[4%] gap-1 mt-4`}>
-        <Text style={tw`text-black dark:text-white font-WorkExtraBold text-2xl`}>
-        Common Questions
+        <Text
+          style={tw`text-black dark:text-white font-WorkExtraBold text-2xl`}>
+          Common Questions
         </Text>
       </View>
       <ScrollView
