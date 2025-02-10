f3696aa31617bc8fb6aa40f97fac8b57a458e309
mdmidulhosen <muhammadmridulhosenkibria@gmail.com>
2025-02-10 14:02:24 +0600

    run the code for ios and wrapped the full application with safeareaview

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry, SafeAreaView, StyleSheet } from 'react-native';
+import { name as appName } from './app.json';
 import AppRoutes from './src/routes/AppRoutes';
+import tw from './src/lib/tailwind';
 
-AppRegistry.registerComponent(appName, () => AppRoutes);
+const MainApp = () => (
+  <SafeAreaView style={tw`flex-1`}>
+    <AppRoutes />
+  </SafeAreaView>
+);
+
+
+AppRegistry.registerComponent(appName, () => MainApp);
